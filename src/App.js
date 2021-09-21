0dc379ec7bf0221a946949021494478fc6512154
Aakash-08 <80558552+Aakash-08@users.noreply.github.com>
2021-09-21 22:45:08 +0530

    new changes

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import HEADSECTION from './Components/Headsection/head'
+import FEATURES from './Components/Features/features'
+import RESTAURANTS from './Components/Restaurants/restaurants'
+import FOOTER from './Components/Footersection/footer'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="body">
+      <HEADSECTION />
+      <FEATURES />
+      <RESTAURANTS />
+      <FOOTER />
+    </div>
+  )
+}
+
+export default App;
