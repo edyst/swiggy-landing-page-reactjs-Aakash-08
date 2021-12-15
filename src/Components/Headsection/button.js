426ee36142d477b30b4e3db9effcd94530fb77c8
Aakash-08 <80558552+Aakash-08@users.noreply.github.com>
2021-12-15 15:26:01 +0530

    last changes

--- src/Components/Headsection/button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './head.css'
-
-const button = (props)=> {
-    return (
-        <button className={props.classes}>{props.name}</button>
-    )
-}
-export default button
